de4afa72db8a51cfff26b81192dfaddb6019d0d7
Jonah Simpson <jonahs99@gmail.com>
2020-10-13 09:39:51 -0400

    Add drawLine

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
-const canvas = document.getElementById('canvas')
-const context = canvas.getContext('2d')
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 
 function onResize() {
-	canvas.width = window.innerWidth
-	canvas.height = window.innerHeight
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 
-	draw(context)
+	draw(context);
 }
 
-window.addEventListener('load', onResize)
-window.addEventListener('resize', onResize)
+window.addEventListener('load', onResize);
+window.addEventListener('resize', onResize);
 
 function draw(ctx) {
 	// clear the canvas
-	ctx.setTransform(1, 0, 0, 1, 0, 0)
-	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+	ctx.setTransform(1, 0, 0, 1, 0, 0);
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
 	// center the canvas	
-	ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2)
+	ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2);
+
+	// styles
+	ctx.strokeStyle = '#333';
+	ctx.lineWidth = 2;
 
 	// TODO: Draw something!
-	ctx.fillStyle = '#ffd500'
-	ctx.beginPath()
-	ctx.arc(0, 0, 72, 0, Math.PI * 2)
-	ctx.fill()
+	drawLine(ctx, 0, 0, 100, 0)
 }
 
+function drawLine(ctx, x1, y1, x2, y2) {
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.stroke();
+}
