c57d116176c28b4077061041ec19b71d208b1eff
Jonah Simpson <jonahs99@gmail.com>
2020-10-21 12:49:45 -0400

    Rearrange functions

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+function draw(ctx) {
+	// clear the canvas
+	ctx.setTransform(1, 0, 0, 1, 0, 0);
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+	// center the canvas
+	ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2);
+
+	// styles
+	ctx.strokeStyle = '#333';
+	ctx.lineWidth = 2;
+
+	// TODO: Draw something else!
+	drawLine(ctx, -200, 0, 200, 0);
+	// drawDragon(ctx, ...);
+}
+
+function drawLine(ctx, x1, y1, x2, y2) {
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.stroke();
+}
+
+function drawDragon(ctx, n, /* ... whatever else you would like :) */) {
+	// TODO
+}
+
+// Some biolerplate to draw on the fullscreen canvas
+
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
@@ -10,26 +40,3 @@ function onResize() {
 
 window.addEventListener('load', onResize);
 window.addEventListener('resize', onResize);
-
-function draw(ctx) {
-	// clear the canvas
-	ctx.setTransform(1, 0, 0, 1, 0, 0);
-	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-	// center the canvas	
-	ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2);
-
-	// styles
-	ctx.strokeStyle = '#333';
-	ctx.lineWidth = 2;
-
-	// TODO: Draw something!
-	drawLine(ctx, 0, 0, 100, 0)
-}
-
-function drawLine(ctx, x1, y1, x2, y2) {
-	ctx.beginPath();
-	ctx.moveTo(x1, y1);
-	ctx.lineTo(x2, y2);
-	ctx.stroke();
-}
